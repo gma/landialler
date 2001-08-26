5198752482e6704cd27f968de998abc60cfcdbe5
graham <>
2001-08-26 10:03:30 +0000

    - added proper documentation. - made the value returned by the api_* methods dependent on the return   value of their external commands.

--- landialler/server/landiallerd.py
+++ landialler/server/landiallerd.py
@@ -1,26 +1,64 @@
 #!/usr/bin/env python
 #
 # landiallerd.py - the LAN dialler daemon
-#
-# Provides several computers on a home LAN with the functionality
-# required to be able to dial up the Internet (e.g. via modem) in
-# situations where only one computer provides connectivity (e.g. via
-# NAT) to a group of workstations.
-#
-# In other words, if you only have one modem and can set your network
-# up so that one computer routes packets between your other computers
-# and the Internet, this software can help you start/stop the
-# connection from the other computers.
-#
-# This daemon runs on the computer that connects directly to the
-# Internet and provides the clients with limited access to the modem,
-# via an XML-RPC API.
+
+"""Landialler Enables several computers on a home LAN to remotely
+control a dial up device (e.g. modem) that is connected to a single
+Unix workstation.
+
+There are two programs that make up landialler; the client and the
+server. This is the server that runs on the Unix workstation.
+
+It runs in the background (as a daemon) waiting for landialler clients
+to connect to it and request an Internet connection (through the
+landialler XML-RPC API). By default it listens for connections on port
+6543.
+
+The client/server API defines three procedures that the client can
+call; connect(), disconnect() and is_connected(). These are
+individually documented in the MyHandler class (see below). Each
+procedure runs an external script/program to perform their task,
+making the landialler server more portable between different versions
+of Unix, or distributions of Linux. Each command should return
+immediately and exit with a non zero return code if there is an
+error. Commands are specified in the [commands] section of the
+landiallerd.conf configuration file.
+
+A sample configuration file should be included with the package, but
+the following should serve as a good example:
+
+  [commands]
+  connect: /usr/local/bin/dialup
+  disconnet: /usr/local/bin/kill-dialup
+  is_connected: /sbin/ifconfig ppp0
+
+  [server]
+  port: 6543
+
+Note that you can also configure the TCP port number that landiallerd
+uses to talk to the clients.
+
+The connect and disconnect scripts above should both make sure that
+they exit immediately; the connect command MUST NOT hang around
+indefinitely waiting to see if the connection has been made, but
+should only check that the commands that it has run have started
+correctly. If you know how to integrate landialler cleanly with your
+own operating system's dial up systems then please send them in and
+they will be made available on the web site (the author only uses
+Debian).
+
+More information on landialler is available at the project home page:
+
+  http://landialler.sourceforge.net/
+
+Author: Graham Ashton <ashtong@users.sourceforge.net>
+
+"""
+
+# $Id$
 #
 # Copyright Graham Ashton <ashtong@users.sourceforge.net>, 2001.
-#
-# $Id$
 
-import ConfigParser
 import gmalib
 import os
 import posixpath
@@ -57,11 +95,10 @@ class MyHandler(xmlrpcserver.RequestHandler):
     def call(self, method, params):
         """Call the XML-RPC procedure and return it's result.
 
-        Calls one of the other methods in this class (which in
-        themselves define the server's API) and returns the other
-        method's return value. If the procedure doesn't exist then an
-        AttributeError is raised, returning a XML-RPC fault to the
-        client.
+        Calls one of the other methods in this class (which define the
+        server's API) and returns the other method's return value. If
+        the procedure doesn't exist then an AttributeError is raised,
+        returning a XML-RPC fault to the client.
 
         """
 
@@ -87,13 +124,17 @@ class MyHandler(xmlrpcserver.RequestHandler):
 
         """
 
-        cmd = "pon"
-        os.system(cmd)
+        config = gmalib.SingleConfigParser()
+        cmd = config.get("commands", "connect")
+        rval = os.system("%s > /dev/null 2>&1" % cmd)
 
-        return xmlrpclib.True
+        if rval == 0:
+            return xmlrpclib.True
+        else:
+            return xmlrpclib.False
 
     def api_disconnect(self, params):
-        """Disconnects from the Internet.
+        """Disconnect from the Internet.
 
         Drops the Internet connection by running an external dial up
         termination script.
@@ -104,30 +145,30 @@ class MyHandler(xmlrpcserver.RequestHandler):
 
         """
 
-        cmd = "poff"
-        os.system(cmd)
+        config = gmalib.SingleConfigParser()
+        cmd = config.get("commands", "disconnect")
+        rval = os.system("%s > /dev/null 2>&1" % cmd)
 
-        return xmlrpclib.True
+        if rval == 0:
+            return xmlrpclib.True
+        else:
+            return xmlrpclib.False
 
     def api_is_connected(self, params):
         """Check if we are connected to the Internet.
 
         Runs an external command to determine whether or not the
-        server is currently dialled up. Currently, the external
-        command prints a 1 to it's stdout if the server is connected,
-        0 otherwise. This behaviour may be changed in a future
-        release, and the command itself will be moved into the
-        configuration file.
+        server is currently dialled up. If the external command exits
+        with a return code of 0 then we return true, otherwise false.
 
         """
 
-        cmd = "/sbin/ifconfig | perl -e 'undef($/); $_ = <STDIN>; " + \
-              "printf \"%s\", /ppp0/? 1 : 0'"
-        fd = os.popen(cmd)
-        is_conn = int(fd.read(1))
-        fd.close()
+        config = gmalib.SingleConfigParser()
+        cmd = config.get("commands", "is_connected")
+        
+        rval = os.system("%s > /dev/null 2>&1" % cmd)
 
-        if is_conn == 1:
+        if rval == 0:
             return xmlrpclib.True
         else:
             return xmlrpclib.False
@@ -137,11 +178,10 @@ class App(gmalib.Daemon):
     """A simple wrapper class that initialises and runs the server."""
     
     def __init__(self):
-        """Calls the base class's initialisor."""
         gmalib.Daemon.__init__(self)
 
-    def run(self):
-        """Start the landiallerd server."""
+    def main(self):
+        """Read configuration, start the XML-RPC server."""
 
         syslog.openlog(posixpath.basename(sys.argv[0]),
                        syslog.LOG_PID | syslog.LOG_CONS)
@@ -149,26 +189,16 @@ class App(gmalib.Daemon):
 
         # load configuration files
         try:
-            self.config = ConfigParser.ConfigParser()
+            self.config = gmalib.SingleConfigParser()
             self.config.read(["/usr/local/etc/landiallerd.conf",
                               "/etc/landiallerd.conf", "landiallerd.conf"])
-            server_ip = self.config.get("server", "ip")
-            server_port = self.config.get("server", "port")
-
-        except ConfigParser.ParsingError, e:
-            self.log_err("Error reading config file: %s" % e)
-            sys.exit()
-
-        except ConfigParser.NoSectionError, e:
-            self.log_err("Error reading config file: %s" % e)
-            sys.exit()
-
-        except ConfigParser.NoOptionError, e:
-            self.log_err("Error reading config file: %s" % e)
+        except Exception, e:
+            self.log_err("Terminating - error reading config file: %s" % e)
             sys.exit()
 
         # start the server and start taking requests
-        self.server = MyTCPServer((server_ip, server_port), MyHandler)
+        server_port = int(self.config.get("server", "port"))
+        self.server = MyTCPServer(("", server_port), MyHandler)
         self.server.serve_forever()
 
         syslog.closelog()
@@ -178,8 +208,7 @@ if __name__ == '__main__':
     if os.name != "posix":
         print "Sorry, only Unix (and similar) operating systems are supported."
         sys.exit()
-
     app = App()
-    # app.be_daemon = 0  # uncomment to run in foreground (easier debugging)
+    app.be_daemon = 0  # uncomment to run in foreground (easier debugging)
     app.debug = 1
-    app.run()
+    app.main()
