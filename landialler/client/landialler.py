6d4badad42b8ee05fee484dfb1025ab5d374b88c
graham <graham@effectif.com>
2001-08-28 15:27:58 +0000

    - added full documentation. - added /usr/local/etc to configuration file search path.

--- landialler/client/landialler.py
+++ landialler/client/landialler.py
@@ -20,16 +20,84 @@
 #
 # $Id$
 
+"""set up a dialup connection on a server
+
+Landialler enables several computers on a home LAN to remotely
+control a dial up device (e.g. modem) that is connected to a single
+Unix workstation.
+
+There are two programs that make up landialler; the client and the
+server. This is the client that runs on any operating system that
+supports Python 2.1 and the Tk Python bindings.
+
+The client and server communicate via XML-RPC. The server runs in the
+background (as a daemon) waiting for landialler clients to connect to
+it and request an Internet connection (through the landialler XML-RPC
+API). By default the server listen for connections on port 6543.
+
+When launched, the client asks the user to confirm that they wish to
+go online (e.g. connect to the Internet). If they do then the client
+asks the server (via XML-RPC) if it is currently connected. If so the
+user is informed that they are already online and the client
+exits. Note: In future the client/server API will support keeping
+track of the number of clients that have requested a connection,
+thereby allowing the server to remain online until it is sure that all
+users have finished with the connection. The current API is documented
+with the landiallerd.py server.
+
+If the server is not online then the client asks the server to connect
+and then checks whether or not the server is connected every five
+seconds, until either the server connects or the timeout period is
+reached (whichever comes first).
+
+Should the timeout period (which is set in the landialler.conf
+configuration file) be reached before the server is online then the
+server is instructed to disconnect from the dial up service and the
+user is given the option of repeating the connection process.
+
+If the server connects successfully then the client displays a dialog
+to the user, prompting them to click a button when they wish to
+disconnect. Once the button is clicked the client instructs the server
+to disconnect, and exits.
+
+The configuration file tells landialler.py which server to connect
+to. A sample configuration file looks like this:
+
+  [xmlrpcserver]
+  hostname: 192.168.1.1  # your Unix box
+  port: 6543             # the default port
+
+  [dialup]
+  timeout: 120           # allow 2 mins to connect, then retry
+
+The configuration file should be called "landialler.conf". On POSIX
+operating systems (e.g. Unix or similar) it can either be placed in
+/usr/local/etc, or the current directory. On other operating systems
+it must be placed in the current directory.
+
+On POSIX operating systems, error, informational and debugging
+messages are written to the syslog.
+
+More information on landialler is available at the project home page:
+
+  http://landialler.sourceforge.net/
+
+The author (Graham Ashton) can be contacted at ashtong@users.sourceforge.net.
+
+"""
+
+
 import ConfigParser
 import gmalib
 import os
-import posixpath
 import socket
 import sys
 import time
 import Tkinter
 import tkMessageBox as tkMBox
 import xmlrpclib
+
+__version__ = "0.1"
 
 
 class UserInterface(gmalib.Logger):
@@ -103,6 +171,10 @@ class App(gmalib.Application):
         try:
             # load config file
             self.config = ConfigParser.ConfigParser()
+            files = []
+            if os.name == "posix":
+                files.append("/usr/local/etc/landialler.conf")
+            files.append("landialler.conf")
             self.config.read("landialler.conf")
             hostname = self.config.get("xmlrpcserver", "hostname")
             port = int(self.config.get("xmlrpcserver", "port"))
