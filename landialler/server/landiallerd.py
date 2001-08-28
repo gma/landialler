d808e0f8eb4f1565ccbab490aaa1a99b388d9005
graham <graham@effectif.com>
2001-08-28 14:53:48 +0000

    - added copyright and GNU disclaimer.

--- landialler/server/landiallerd.py
+++ landialler/server/landiallerd.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 #
 # landiallerd.py - the LAN dialler daemon
+#
+# Copyright (C) 2001 Graham Ashton
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# $Id$
 
 """Landialler Enables several computers on a home LAN to remotely
 control a dial up device (e.g. modem) that is connected to a single
@@ -55,9 +73,6 @@ Author: Graham Ashton <ashtong@users.sourceforge.net>
 
 """
 
-# $Id$
-#
-# Copyright Graham Ashton <ashtong@users.sourceforge.net>, 2001.
 
 import gmalib
 import os
