428b7b971830cf98baed4ff644a040bfac0adffa
graham <graham@effectif.com>
2001-10-16 17:10:56 +0000

    - added spacing between buttons in Window.draw_buttons(). - made long text in dialog's more readable by using the Message()   class. - completed update() and cleanup() methods for dialogs. - added DroppedDialog class. - added MainWindow.status_check() for repeated status updates.

--- landialler/client/landiallermvc/tkviews.py
+++ landialler/client/landiallermvc/tkviews.py
@@ -63,8 +63,8 @@ class Window:
         # element in the tuple is for). Before we can sort them we
         # need to put them into a list.
         #
-        # FIXME: there must be a better way of sorting a hash by it's
-        # values than this, surely...
+        # FIXME: There must be a better way of sorting a hash by it's
+        # values than this, perhaps with a sub class of UserDict().
 
         button_bar = []
         for name in self.buttons.keys():
@@ -73,14 +73,20 @@ class Window:
             button_bar.append((name, position, callback))
 
         if len(button_bar) > 1:
-            button_bar.sort(lambda a, b: cmp(a[1], b[1]))
+            button_bar.sort(lambda a, b: cmp(b[1], a[1]))
 
+        count = 0
         for tuple in button_bar:
             (name, pos, cmd) = tuple
             button = Button(frame, text=name.capitalize(), command=cmd)
-            button.pack(side=self.button_side)
             key = name.lower()
             self.button_store[key] = button  # so we can call btn.config()
+
+            if count % 2: padding = 6
+            else:         padding = 0
+            button.pack(side=self.button_side, padx=padding)
+            count += 1
+
         frame.pack(side=self.button_side)
 
 
@@ -92,7 +98,6 @@ class Dialog(Window):
         """Set default button_side attribute to TOP."""
         Window.__init__(self)
         self.modal = 0
-        self.button_side = TOP
         self.win = None
 
     def draw(self):
@@ -101,15 +106,17 @@ class Dialog(Window):
         self.win.title(self.title)
         self.win.protocol('WM_DELETE_WINDOW', lambda: 0)  # ignore close button
         frame = Frame(self.win, bd=6)
-        if len(self.text) > 30:
-            widget = Message(frame, text=self.text)
+        max_cols = 15
+        max_pixels = 160
+        if len(self.text) > max_cols:
+            widget = Message(frame, text=self.text, width=max_pixels)
         else:
-            widget = Label(frame, text=self.text)
+            widget = Label(frame, text=self.text, width=len(self.text))
         widget.pack()
         frame.pack()
         self.draw_buttons(self.win)
+        self.win.focus_set()
         if self.modal:
-            self.win.focus_set()
             self.win.grab_set()
             self.win.wait_window()
 
@@ -124,20 +131,51 @@ class ConnectingDialog(Dialog, views.ConnectingDialog):
         global root
         self.win.destroy()
         root.quit()
+
+    def update(self):
+        """Closes the dialog once the connection is made."""
+        if self.model.is_connected:
+            self.win.destroy()
     
 
 class DisconnectDialog(Dialog, views.DisconnectDialog):
     def __init__(self, model):
         Dialog.__init__(self)
         views.DisconnectDialog.__init__(self, model)
+        self.modal = 1
+
+    def cleanup(self):
+        """Cleans up after the Yes or No buttons have been pressed.
+
+        Destroys the dialog box.
+
+        """
+        self.win.destroy()
+
+
+class DroppedDialog(Dialog, views.DroppedDialog):
+    def __init__(self, model):
+        Dialog.__init__(self)
+        views.DroppedDialog.__init__(self, model)
+        self.modal = 1
+
+    def cleanup(self):
+        """Cleans up after the OK button has been pressed.
+
+        Exits the application.
+
+        """
+        global root
+        self.win.destroy()
+        root.quit()
 
 
 class MainWindow(Window, views.MainWindow):
     def __init__(self, model):
         Window.__init__(self)
         views.MainWindow.__init__(self, model)
-        self.default_var = []   # stores variable values (i.e. actual status)
-        self.update_var = []  # StringVar() object's for auto label updating
+        self.default_var = []  # stores variable values (i.e. actual status)
+        self.update_var = []   # StringVar() object's for auto label updating
         for row in self.status_rows:
             (label, value) = row
             self.default_var.append(value)
@@ -156,6 +194,7 @@ class MainWindow(Window, views.MainWindow):
         root.protocol('WM_DELETE_WINDOW', on_delete_cb)
         self.draw_labels()
         self.draw_buttons()
+        self.status_check()
         self.update()  # update labels immediately
         mainloop()
 
@@ -180,14 +219,25 @@ class MainWindow(Window, views.MainWindow):
         frame2.pack()
         frame1.pack()
 
+    def status_check(self):
+        """Regularly check the connection status on the server.
+
+        Calls the model's get_server_status() method every
+        status_check_period milliseconds.
+
+        """
+        global root
+        self.model.get_server_status()
+        root.after(self.model.status_check_period, self.status_check)  # re-run
+
     def update(self):
         """Updates the status display."""
+        self.update_var[1].set(self.model.current_users)
         if self.model.is_connected:
             self.update_var[0].set('Online')
-            if self.button_store.has_key('disconnect'):
-                self.button_store['disconnect'].config(state=ACTIVE)
+            self.button_store['disconnect'].config(state=ACTIVE)
         else:
             self.update_var[0].set('Offline')
-            if self.button_store.has_key('disconnect'):
-                self.button_store['disconnect'].config(state=DISABLED)
-        self.update_var[1].set(self.model.current_users)
+            self.button_store['disconnect'].config(state=DISABLED)
+            dialog = DroppedDialog(self.model)
+            dialog.draw()
