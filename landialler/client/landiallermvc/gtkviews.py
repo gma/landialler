e25bf292bc77326fd61aa9529ca7c4a53dc077f6
graham <>
2001-10-18 11:14:53 +0000

    - more progress.

--- landialler/client/landiallermvc/gtkviews.py
+++ landialler/client/landiallermvc/gtkviews.py
@@ -24,20 +24,68 @@
 """implements the GTK+ landialler user interface"""
 
 
+##from _gtk import *
+##from GTK import *
+from gtk import *
 import views
 
 
-class Dialog(views.View):
-    pass
+class Dialog:
+    def __init__(self):
+        self.window = None
+
+    def destroy_cb(self, *args):
+        self.window.hide()
+        mainquit()
+
+    def draw(self):
+        """Displays the dialog's message and buttons."""
+        self.window = win = GtkWindow(WINDOW_DIALOG)
+        win.connect('destroy', self.destroy_cb)
+        win.set_border_width(8)
+        win.set_title(self.title)
+        self.draw_buttons()
+        win.show()
+        mainloop()
+
+    def draw_buttons(self):
+        button = GtkButton("Disconnect")
+        button.connect("clicked", self.button_cb)
+        self.window.add(button)
+        button.show()
 
 
 class ConnectingDialog(Dialog, views.ConnectingDialog):
-    pass
+    def __init__(self, model):
+        Dialog.__init__(self)
+        views.ConnectingDialog.__init__(self, model)
+
+    def button_cb(self, *args):
+        print "button_cb ..."
 
 
 class DisconnectDialog(Dialog, views.DisconnectDialog):
-    pass
+    def __init__(self, model):
+        Dialog.__init__(self)
+        views.DisconnectDialog.__init__(self, model)
 
 
 class MainWindow(views.MainWindow):
-    pass
+    def __init__(self, model):
+        views.MainWindow.__init__(self, model)
+
+    def cleanup(self):
+        """Destroy the main window."""
+        print "FIXME: call server_disonnect from cleanup()"
+
+    def draw(self):
+        """Display the main window."""
+        pass
+
+    def draw_labels(self):
+        """Draws frame containing status display."""
+        pass
+
+    def draw_buttons(self):
+        """Lays out a set of buttons in a button bar."""
+        pass
