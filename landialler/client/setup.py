26483ef27ad20d0f0d2a03a50b4daf6275b16532
graham <>
2001-10-18 12:47:03 +0000

    - new file.

---
+++ landialler/client/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# setup.py - landialler setup/install script
+#
+# Usage: python setup.py install
+#
+# $Id$
+
+from distutils.core import setup
+
+setup(name="landialler",
+      version="0.2pre1",
+      description="LANdialler client",
+      author="Graham Ashton",
+      author_email="ashtong@users.sourceforge.net",
+      url="http://landialler.sourceforge.net/",
+      packages=['landialler'])
