13acc465272f367c6f7ef89d90e77662995b777e
graham <graham@effectif.com>
2001-10-19 12:22:43 +0000

    - new file.

---
+++ gmalib/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# setup.py - gmalib setup/install script
+#
+# Usage: python setup.py install
+#
+# $Id$
+
+from distutils.core import setup
+
+setup(name="gmalib",
+      version="0.2",
+      description="Library of useful classes for the author's applications",
+      author="Graham Ashton",
+      author_email="ashtong@users.sourceforge.net",
+      url="http://landialler.sourceforge.net/",
+      py_modules=['gmalib'])
