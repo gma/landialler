51ad08c83de7d0d6c4c1cf72804c30f7492979f5
graham <graham@effectif.com>
2001-10-29 12:28:31 +0000

    - added support for logging to normal files. - Daemon is currently broken.

--- gmalib/gmalib.py
+++ gmalib/gmalib.py
@@ -26,133 +26,134 @@ the average application or daemon is included.
 
 import ConfigParser
 import os
+import posixpath
 import stat
 import sys
+import time
+
 try:
     import syslog
 except ImportError, e:
     if os.name == "posix":
-        print "can't import syslog: %s" % e
+        sys.stderr.write("can't import syslog: %s" % e)
 
 
 __version__ = "0.2"
 
 
 class Logger:
+
     """Provides generic (and very basic) logging functionality.
 
-    Provides a sub class with a methods which write to the
-    syslog. When run on non POSIX operating systems it is assumed that
-    syslog is unavailable and output is redirected to STDOUT instead.
-
-    The code that calls Logger methods should set the facility by a
-    call to syslog.openlog() from within the calling program or sub
-    class.
-
-    Public attributes:
-
-    debug -- Debug messages are logged when true (default is false)
+    Designed to be used as a mixin class, Logger provides objects with 
+    methods for logging messages either to a file, or to syslog.
 
     """
 
-    def __init__(self):
+    # class vars used for creating log time stamps
+    weekdayname = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    monthname = [None,
+                 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+    def __init__(self, file=None, use_syslog=0):
         """Initialises the object.
 
-        Sets debug and log_to_console attributes to false by default.
+        If specified, the file attribute should be the name of a file to
+        append log messages to. If the syslog attribute is true then
+        messages will be written to syslog, on POSIX systems.
 
         """
-
+        # default attributes
+        self.file = file
+        self.use_syslog = use_syslog
         if not hasattr(self, 'debug'):
             self.debug = 0
 
-        # log to console if there's no syslog or we're debugging
-        if (not os.name == "posix") or self.debug:
-            self.log_to_console = 1
-        else:
-            self.log_to_console = 0
+        if self.use_syslog:
+            syslog.openlog(posixpath.basename(sys.argv[0]),
+                syslog.LOG_PID | syslog.LOG_CONS)
+    
+    def __del__(self):
+        """Close syslog if it's in use."""
+        if self.use_syslog:
+            syslog.closelog()
+
+    def log_date_time_string(self):
+        """Return the current time formatted for logging."""
+        now = time.time()
+        year, month, day, hh, mm, ss, x, y, z = time.localtime(now)
+        s = "%02d/%3s/%04d %02d:%02d:%02d" % \
+            (day, self.monthname[month], year, hh, mm, ss)
+        s = "%3s %02d %04d %02d:%02d:%02d" % \
+            (self.monthname[month], day, year, hh, mm, ss)
+        return s
+
+    def _to_file(self, priority, message):
+        f = open(self.file, "a")
+        date_time = self.log_date_time_string()
+        f.write("%s [%s]: %s\n" % (date_time, priority.upper(), message))
+        f.close()
 
     def log_debug(self, message):
-        """Write message to syslog with LOG_DEBUG priority.
+        """Log debug message."""
 
-        If the object's debug attribute is true then the message is
-        written syslog with a priority of LOG_DEBUG.
-        
-        """
-
-        if os.name == "posix":
-            if self.debug:
+        if self.debug:
+            if self.use_syslog:
                 syslog.syslog(syslog.LOG_DEBUG, message)
-
-        if self.log_to_console:
-            print message
+            if self.file:
+                self._to_file("debug", message)
 
     def log_info(self, message):
-        """Write message to syslog with LOG_INFO priority."""
-
-        if os.name == "posix":
+        """Log info message."""
+        if self.use_syslog:
             syslog.syslog(syslog.LOG_INFO, message)
-
-        if self.log_to_console:
-            print message
+        if self.file:
+            self._to_file("info", message)
 
     def log_notice(self, message):
-        """Write message to syslog with LOG_NOTICE priority."""
-
-        if os.name == "posix":
+        """Log a notice message."""
+        if self.use_syslog:
             syslog.syslog(syslog.LOG_NOTICE, message)
-
-        if self.log_to_console:
-            print message
+        if self.file:
+            self._to_file("notice", message)
 
     def log_err(self, message):
-        """Write message to syslog with LOG_ERR priority."""
-
-        if os.name == "posix":
+        """Log an error message."""
+        if self.use_syslog:
             syslog.syslog(syslog.LOG_ERR, message)
-
-        if self.log_to_console:
-            print message
-
-
-class Application(Logger):
-    """A framework that provides generic application functionality.
-
-    Currently, it's empty.
-
-    """
-    
-    def __init__(self):
-        """Calls the base class's initialiser."""
-        
-        Logger.__init__(self)
+        if self.file:
+            self._to_file("error", message)
 
 
-class Daemon(Application):
-    """Provides an Application with simple daemon functionality (UNIX only).
+class Daemon:
+
+    """Provides an application with basic daemon functionality (UNIX only).
 
     Any script that needs to run as a daemon (a background process)
     need only be written in an object oriented manner (e.g. wrap it up
     in a run() method, or similar), and specify this class as a base
-    class.
+    class. Treat it as a mixin class.
 
     A call to self.daemonise() will convert the calling script into a
-    daemon, unless it's be_daemon attribute is false (it defaults to
-    true).
-
-    Publically accessible attributes:
-
-    be_daemon -- Run as a daemon when true (the default)
+    daemon. The sys.stdout and sys.stderr file handles are re-directed
+    via a Logger object that 
 
     """
     
-    def __init__(self):
-        """Initialises the base class and sets self.be_daemon to true."""
+    def __init__(self, **log_args):
+        """Initialistion.
         
-        Application.__init__(self)
-        self.be_daemon = 1			# run as daemon by default
+        Keyword arguments passed via the **log_args argument are passed
+        straight into the Logger object that sys.stdout and sys.stderr
+        are redirected to, so can be used to configure the logging
+        options.
+        
+        """
+        self.log_args = log_args
 
     def daemonise(self):
-        """Convert the caller into a daemon (UNIX only).
+        """Convert the caller into a daemon (POSIX only).
 
         Forks a child process, exits the parent, makes the child a
         session leader and sets the umask to 0 (so programs run from
@@ -161,22 +162,14 @@ class Daemon(Application):
         respectively).
 
         """
-
         # UNIX only, sorry.
-
         if os.name != "posix":
-            print "unable to run as a daemon (Unix only)"
-            return None
-
-        # Optionally run in the foreground (handy for debugging).
-
-        if not self.be_daemon:
+            print "unable to run as a daemon (POSIX only)"
             return None
 
         # See "Python Standard Library", pg. 29, O'Reilly, for more
         # info on the following.
-        
-        self.log_debug("converting to a daemon")
+        print "converting to a daemon"
         pid = os.fork()
         if pid:
             os._exit(0) # kill parent
@@ -185,17 +178,16 @@ class Daemon(Application):
         os.umask(0)
 
         # Redirect stdout and stderr to syslog.
-
         class _StdOutLogDevice(Logger):
             def __init__(self):
-                Logger.__init__(self)
+                Logger.__init__(self, self.log_args)
                 
             def write(self, msg):
                 self.log_info(msg)
 
         class _StdErrLogDevice(Logger):
             def __init__(self):
-                Logger.__init__(self)
+                Logger.__init__(self, self.log_args)
                 
             def write(self, msg):
                 self.log_err(msg)
@@ -237,14 +229,23 @@ class SharedConfigParser(ConfigParser.ConfigParser):
             self.already_read = 1
 
 
-if __name__ == '__main__':
-    import posixpath
+def test():
     if "syslog" in dir():
         syslog.openlog(posixpath.basename(sys.argv[0]))
-    d = Daemon()
-    d.debug = 1
-    print "converting to daemon... ",
-    d.daemonise()
-    print "done.",  # shouldn't see this!
-    if "syslog" in dir():
-        syslog.closelog()
+    
+    log = Logger(file="test.log", use_syslog=1)
+    log.log_info("an info log message")
+    log.log_err("an error log message")
+    log.log_notice("a notice log message")
+    #d = Daemon()
+    #d.debug = 1
+    #print "converting to daemon... ",
+    #d.daemonise()
+    #print "done.",  # shouldn't see this!
+    #if "syslog" in dir():
+    #    syslog.closelog()
+
+
+
+if __name__ == '__main__':
+    test()
