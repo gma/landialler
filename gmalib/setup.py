ea8c860e899ce563a32e1b2f51d2374f0bd3d3f1
graham <graham@effectif.com>
2001-10-29 20:38:23 +0000

    - bumped version number to 0.3pre1.

--- gmalib/setup.py
+++ gmalib/setup.py
@@ -9,7 +9,7 @@
 from distutils.core import setup
 
 setup(name="gmalib",
-      version="0.2",
+      version="0.3pre1",
       description="Library of useful classes for the author's applications",
       author="Graham Ashton",
       author_email="ashtong@users.sourceforge.net",
