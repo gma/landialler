1ac600b6fcce86e68a465e09bd129694bb1a5aa9
graham <graham@effectif.com>
2001-10-31 09:39:30 +0000

    - moved to version 0.2.2.

--- gmalib/setup.py
+++ gmalib/setup.py
@@ -9,7 +9,7 @@
 from distutils.core import setup
 
 setup(name="gmalib",
-      version="0.2.1",
+      version="0.2.2",
       description="Library of useful classes for the author's applications",
       author="Graham Ashton",
       author_email="ashtong@users.sourceforge.net",
