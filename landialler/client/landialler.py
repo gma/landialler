30273fe322c100e4fd9cbb0ab559690899c61d9a
graham <>
2001-11-12 00:27:38 +0000

    - refactored Model into it's own file.

--- landialler/client/landialler.py
+++ landialler/client/landialler.py
@@ -87,6 +87,8 @@ import exceptions
 import ConfigParser
 import getopt
 import gmalib
+import landiallermvc
+from landiallermvc import Model
 import os
 import socket
 import sys
@@ -94,132 +96,6 @@ import xmlrpclib
 
 
 __version__ = "0.2"
-
-
-class ConnectError(exceptions.Exception):
-
-    """Raised if the remote connect procedure fails."""
-    
-    def __init__(self, args=None):
-        self.args = args
-
-
-class DisconnectError(exceptions.Exception):
-
-    """Raised if the remote disconnect procedure fails."""
-
-    def __init__(self, args=None):
-        self.args = args
-
-
-class StatusError(exceptions.Exception):
-
-    """Raised if the remote get_status procedure fails."""
-
-    def __init__(self, args=None):
-        self.args = args
-
-
-class Model:
-
-    """The Model component of the Model-View-Controller pattern.
-
-    See [POSA125] for more information.
-
-    """
-
-    def __init__(self, config, server, toolkit):
-        self.config = config  # ConfigParser object
-        self.server = server  # connection to XML-RPC server
-        self._observers = []  # observers are MVC views or controllers
-        self.check_status_period = 5000  # msec between get_server_status()
-        self.toolkit = toolkit # name of toolkit (lowercase), e.g. "tk"
-        self.views = None     # name of toolkit support module (e.g. "tkviews")
-        self.choose_gui_toolkit()
-        
-        # server status attributes (retrieved via RPC)
-        self.is_connected = 0
-        self.current_users = 0
-        
-        # attributes for maintaining state
-        self.was_connected = 0  # set to 1 if we have been online
-
-    def attach(self, observer):
-        """Attachs an observer to the publish-subscribe mechanism."""
-        self._observers.append(observer)
-
-    def detach(self, observer):
-        """Detachs an observer from the publish-subscribe mechanism."""
-        i = 0
-        for obs in self._observers:
-            if obs is observer:
-                del self._observers[i]
-            i += 1
-
-    def choose_gui_toolkit(self):
-        """Work out which GUI toolkit the View components should use.
-        
-        Imports the correct view module (e.g. tkviews) and makes
-        self.toolkit an alias to the module.
-        
-        """
-        if not self.toolkit:  # i.e. if not already set on command line
-            try:
-                self.toolkit = self.config.get("interface", "toolkit")
-            except ConfigParser.Error:
-                if os.name == "posix":
-                    self.toolkit = "gtk"
-                else:
-                    self.toolkit = "tk"
-
-        try:
-            exec("from landiallermvc import %sviews" % self.toolkit)
-            exec("self.views = %sviews" % self.toolkit)
-        except ImportError:
-            exec("from landiallermvc import %sviews" % "tk")
-            exec("self.views = %sviews" % "tk")
-
-    def notify(self):
-        """Calls each observer's update() method."""
-        for observer in self._observers:
-            observer.update()
-
-    def get_server_status(self):
-        """Retrieves the status of the server's connection.
-
-        Calls the XML-RPC API's get_status() method, then informs all
-        observers by calling the notify() method.
-        
-        """
-        (self.current_users, self.is_connected) = self.server.get_status()
-        self.notify()
-        if self.is_connected == 1 and self.was_connected == 0:
-            self.was_connected = 1  # can now determine if connection dropped
-    
-    def server_connect(self):
-        """Instructs the server to start the dial up connection.
-        
-        Calls the XML-RPC API's connect() method. Raises a
-        ConnectError exception if the connect() method returns false.
-        
-        """
-        rval = self.server.connect()
-        if rval.value == xmlrpclib.False:
-            raise ConnectError
-
-    def server_disconnect(self, all="no"):
-        """Instructs the server to disconnect the dial up connection.
-        
-        Calls the XML-RPC API's disconnect() method. Returns 1 if the
-        server reported that it's disconnect command exited successfully,
-        0 otherwise.
-        
-        """
-        rval = self.server.disconnect(all)
-        if rval.value == xmlrpclib.True:
-            return 1
-        else:
-            return 0
 
 
 class App(gmalib.Logger):
@@ -283,13 +159,13 @@ class App(gmalib.Logger):
         
             server = xmlrpclib.Server("http://%s:%s/" % (hostname, port))
             self.log_debug("connected to %s:%s" % (hostname, port))
-            self.model = Model(config, server, self.toolkit)
+            self.model = Model.Model(config, server, self.toolkit)
             window = self.model.views.MainWindow(self.model)
-
+        
         except Exception, e:
             print e
             sys.exit(1)
-
+        
         # from now on we can do GUI based error messages
         try:
             window.draw()
@@ -300,11 +176,11 @@ class App(gmalib.Logger):
                 self.model.server_connect()
             window.start_event_loop()
         
-        except ConnectError:
+        except landiallermvc.ConnectError:
             self.handle_connect_error()
-        except DisconnectError:
+        except landiallermvc.DisconnectError:
             self.handle_disconnect_error()
-        except StatusError:
+        except landiallermvc.StatusError:
             self.handle_status_error()
         except socket.error, e:
             self.handle_socket_error(e)
