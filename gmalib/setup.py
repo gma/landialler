dc01fe56fe1b1a7516aa1b9e9d62ce06be663aff
ashtong <>
2003-09-13 12:21:55 +0000

    - get version number from gmalib.py. - coding style.

--- gmalib/setup.py
+++ gmalib/setup.py
@@ -6,12 +6,17 @@
 #
 # $Id$
 
-from distutils.core import setup
 
-setup(name="gmalib",
-      version="0.2.2",
-      description="Library of useful classes for the author's applications",
-      author="Graham Ashton",
-      author_email="ashtong@users.sourceforge.net",
-      url="http://landialler.sourceforge.net/",
-      py_modules=['gmalib'])
+import distutils.core
+
+import gmalib
+
+
+if __name__ == '__main__':
+    distutils.core.setup(name="gmalib",
+                         version=gmalib.__version__,
+                         description="cused by author's own applications",
+                         author="Graham Ashton",
+                         author_email="ashtong@users.sourceforge.net",
+                         url="http://landialler.sourceforge.net/",
+                         py_modules=['gmalib'])
